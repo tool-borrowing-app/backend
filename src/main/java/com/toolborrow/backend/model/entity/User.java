38bd7148bb2cb3236f13e4d1fc6730e38568af08
Daniel Spaits <danielspaits22@gmail.com>
2025-12-26 13:39:47 +0100

    add imports i messed up by conflict

--- src/main/java/com/toolborrow/backend/model/entity/User.java
+++ src/main/java/com/toolborrow/backend/model/entity/User.java
@@ -1,9 +1,7 @@
 package com.toolborrow.backend.model.entity;
 
 import com.toolborrow.backend.model.entity.base.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
